4c8f05760437ab72f14d4bde4538628e501a2f3b
Wusul Platform <57298692+WusulPlatform@users.noreply.github.com>
2019-11-17 16:35:59 +0300

    Add files via upload

---
+++ js/sub.js
@@ -0,0 +1,28 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+  document.getElementById("main").style.display="none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("main").style.display="block";
+}
